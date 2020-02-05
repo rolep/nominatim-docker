9c73cf720e7697d65a0cda9117e9b2e247fca45d
Oleg Russkin <oleg@rolep.me>
2020-02-05 18:46:54 +0200

    use default db path

--- 3.4/init.sh
+++ 3.4/init.sh
@@ -2,16 +2,9 @@ OSMFILE=$1
 PGDIR=$2
 THREADS=$3
 
-mkdir -p /data/$PGDIR && \
-
-chown postgres:postgres /data/$PGDIR && \
-
-export  PGDATA=/data/$PGDIR  && \
-sudo -u postgres /usr/lib/postgresql/11/bin/initdb -D /data/$PGDIR && \
-sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR start && \
+sudo -u postgres /usr/lib/postgresql/11/bin/initdb && \
+sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl start && \
 # set data_directory in config for psql - delete current entry and add new one
-sed -i '/^data_directory/d' /etc/postgresql/11/main/postgresql.conf && \
-echo "data_directory = '/data/$PGDIR'" >> /etc/postgresql/11/main/postgresql.conf && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
@@ -24,6 +17,5 @@ else
   sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS
 fi
 
-sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR stop && \
-sed -i '/^data_directory/d' /etc/postgresql/11/main/postgresql.conf && \
-sudo chown -R postgres:postgres /data/$PGDIR
+sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl stop && \
+sudo chown -R postgres:postgres $PGDATA
